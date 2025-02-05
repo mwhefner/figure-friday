ce57eef2aaae3342192706b7ddc245fd44981f6f
M. W. Hefner <103405924+mwhefner@users.noreply.github.com>
2025-02-05 16:12:53 -0500

    Update ff_2025_05.py
    Made the pattern color always match the leaf color.

--- Week 5/ff_2025_05.py
+++ Week 5/ff_2025_05.py
@@ -162,8 +162,8 @@ def create_sunburst(unitRadios, tagsLayers, currencyRadios, priceSlider):
     
     sunburst['colors'] = ["#000000" if i % 2 == 0 else "#FFFFFF" for i in range(len(df_filtered))] + ["#000000" if i % 2 == 0 else "#FFFFFF" for i in range(len(sunburst) - len(df_filtered))]
 
-    sunburst['pattern_shape_sequence'] = ["/" if i % 2 == 0 else "\\" for i in range(len(sunburst))]
-    sunburst['bgcolor'] = ["#333" if i % 2 == 0 else "#EEE" for i in range(len(sunburst))]
+    sunburst['pattern_shape_sequence'] = ["/" if color == "#000" else "\\" for color in sunburst['colors']]
+    sunburst['bgcolor'] = ["#333" if color == "#000" else "#EEE" for color in sunburst['colors']]
     
     # Make the figure
     fig = go.Figure(
